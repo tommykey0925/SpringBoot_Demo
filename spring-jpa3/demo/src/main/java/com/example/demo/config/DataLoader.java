1479d768929a682df6c48f6cc5fabee7b8df57a3
tommykey0925 <71512581+tommykey0925@users.noreply.github.com>
2023-05-15 15:28:35 +0900

    :recycle: refactor: 初期データを追加 :recycle: refactor: mybatisを使うように修正

--- spring-jpa3/demo/src/main/java/com/example/demo/config/DataLoader.java
+++ spring-jpa3/demo/src/main/java/com/example/demo/config/DataLoader.java
@@ -5,27 +5,48 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import com.example.demo.model.Department;
+import com.example.demo.model.Employee;
 import com.example.demo.repository.DepartmentRepository;
+import com.example.demo.repository.EmployeeRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Component
 public class DataLoader implements ApplicationRunner{
-  private final DepartmentRepository repository;
+
+  private final DepartmentRepository dr;
+  private final EmployeeRepository er;
 
   @Override
   public void run(ApplicationArguments args) throws Exception{
+    //setInitDepartment
     var sales = new Department();
     sales.setName("営業");
-    repository.save(sales);
+    dr.save(sales);
 
     var development = new Department();
     development.setName("開発");
-    repository.save(development);
+    dr.save(development);
 
     var generalAffairs = new Department();
     generalAffairs.setName("総務");
-    repository.save(generalAffairs);
+    dr.save(generalAffairs);
+
+    //setInitEmployee
+    var satou = new Employee();
+    satou.setName("佐藤");
+    satou.setDepartment(sales);
+    er.save(satou);
+    
+    var suzuki = new Employee();
+    suzuki.setName("鈴木");
+    suzuki.setDepartment(development);
+    er.save(suzuki);
+
+    var takahashi = new Employee();
+    takahashi.setName("高橋");
+    takahashi.setDepartment(generalAffairs);
+    er.save(takahashi);
   }
 }
